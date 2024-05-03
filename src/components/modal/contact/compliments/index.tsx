74b0b7dc04a5b2673a6c982920f672807278ed7a
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-05-03 11:34:16 -0300

    contato

--- src/components/modal/contact/compliments/index.tsx
+++ src/components/modal/contact/compliments/index.tsx
@@ -5,11 +5,9 @@ import Button from "../../../button";
 export default function Compliments({
   setSubmit,
   formType,
-  windowHeight,
 }: {
   setSubmit: any;
   formType: string;
-  windowHeight: number;
 }) {
   const initialFormState = useMemo(
     () => ({
